e6833e2cb820758191ace181c36dbbfad8c1fbdd
tintokaka <tintokaka@gmail.com>
2023-02-10 00:39:50 -0800

    .

--- training.py
+++ training.py
@@ -15,18 +15,24 @@ lemmatizer = WordNetLemmatizer()
 
 intents = json.loads(open("intents.json").read())
 
+
 words = []
 classes = []
 documents = []
-ignore_letter = ['!', '?', '.', ',']
+ignore_letters = ['!', '?', '.', ',']
 
-for intent in intents['intents']:
+for intent in intents["intents"]:
     for pattern in intent["patterns"]:
         word_list = nltk.word_tokenize(pattern)
         words.extend(word_list)
-        documents.append(word_list)
-        
+        documents.append((word_list, intent["tag"]))
+        if intent["tag"] not in classes:
+            classes.append(intent["tag"])
+            
+print(words)
+words = [lemmatizer.lemmatize(word.lower()) for word in words if word not in ignore_letters]
+words = sorted(set(words))
+classes = sorted(set(words))
 
-print(words, '\n', documents)
-
- 
+pickle.dump(words, open('words.pkl'), 'wb')
+pickle.dump(classes, open('classes.pkl'), 'wb')
